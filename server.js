224b49cbc8229ad48822f56945a5f439c2a4f8bc
ikuratakashi <ikura1108@icloud.com>
2018-05-25 23:55:05 +0900

    2つのソースを作成

--- server.js
+++ server.js
@@ -4,3 +4,25 @@ var http = require('http');
 //HTTPサーバを作成
 var server = http.createServer();
 var io = require('socket.io').listen(server);
+
+server.on('request', function(req,res){
+    fs.readFile('client.html', function (err,data){
+        if(err){
+            res.writeHead(500);
+            return res.end('Error loading client.html');
+        }
+        res.writeHead(200,{
+            'Content-Type' : 'text/html; charset=UTF-8'
+        });
+        res.end(data);
+    });
+});
+server.listen(8080);
+
+//リクエスト受診時の処理
+io.sockets.on('connection',function(socket){
+    //greeting イベントを発生させる
+    socket.emit('greeting',{message:'hello,  '}, function (data){
+        console.log('request:   ' + data);
+    });
+});
